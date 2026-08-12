9d8b22f4503737df432c707b6ba9a04f9ef4dff8
Thanishmaa-Saravanan <thanishmaasaravanan@gmail.com>
2026-08-12 11:44:53 +0530

    Create 2154-MinimumMovesToConvertString

---
+++ 2154-MinimumMovesToConvertString/2154-MinimumMovesToConvertString.java
@@ -0,0 +1,15 @@
+// Last updated: 12/08/2026, 11:44:53
+class Solution {
+    public int minimumMoves(String s) {
+        int moves = 0;
+        for(int i=0;i<s.length();){
+            if(s.charAt(i)=='X'){
+                moves++;
+                i+=3;
+            }else{
+                i++;
+            }
+        }
+        return moves;
+    }
+}
