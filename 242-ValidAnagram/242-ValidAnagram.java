3ec3a1e14d97549bf935246b1649420fe86f4783
Thanishmaa-Saravanan <thanishmaasaravanan@gmail.com>
2026-08-12 11:47:44 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,20 @@
+// Last updated: 12/08/2026, 11:47:44
+import java.util.*;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        
+        int[] count = new int[26]; 
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        
+        for (int c : count) {
+            if (c != 0) return false;
+        }
+        
+        return true;
+    }
+}
