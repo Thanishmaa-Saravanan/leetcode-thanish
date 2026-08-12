a2072f7ddf963af7a56dc11d6be9b98081561e26
Thanishmaa-Saravanan <thanishmaasaravanan@gmail.com>
2026-08-12 11:47:46 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -0,0 +1,15 @@
+// Last updated: 12/08/2026, 11:47:45
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+}
