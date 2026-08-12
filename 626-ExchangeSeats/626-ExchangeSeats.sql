cb5cc8011780211620d492cb2bb36b111578c100
Thanishmaa-Saravanan <thanishmaasaravanan@gmail.com>
2026-08-12 11:46:38 +0530

    Create 626-ExchangeSeats

---
+++ 626-ExchangeSeats/626-ExchangeSeats.sql
@@ -0,0 +1,12 @@
+-- Last updated: 12/08/2026, 11:46:37
+SELECT 
+    CASE 
+        WHEN id % 2 = 1 AND id + 1 <= (SELECT COUNT(*) FROM Seat) 
+            THEN id + 1
+        WHEN id % 2 = 0 
+            THEN id - 1
+        ELSE id
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
