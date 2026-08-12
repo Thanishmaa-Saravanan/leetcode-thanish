d4e553f1dea294c67775d31c4da524ccfabf5f05
Thanishmaa-Saravanan <thanishmaasaravanan@gmail.com>
2026-08-12 11:46:48 +0530

    Create 506-RelativeRanks

---
+++ 506-RelativeRanks/506-RelativeRanks.java
@@ -0,0 +1,34 @@
+// Last updated: 12/08/2026, 11:46:48
+import java.util.*;
+
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        int n = score.length;
+        String[] result = new String[n];
+
+        // Pair scores with their indices
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = score[i];
+            arr[i][1] = i;
+        }
+
+        // Sort by score descending
+        Arrays.sort(arr, (a, b) -> b[0] - a[0]);
+
+        // Assign ranks
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                result[arr[i][1]] = "Gold Medal";
+            } else if (i == 1) {
+                result[arr[i][1]] = "Silver Medal";
+            } else if (i == 2) {
+                result[arr[i][1]] = "Bronze Medal";
+            } else {
+                result[arr[i][1]] = String.valueOf(i + 1);
+            }
+        }
+
+        return result;
+    }
+}
